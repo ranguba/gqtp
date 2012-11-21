581b22457ad196595fc2e621ad490adc7daf89f4
Kouhei Sutou <kou@clear-code.com>
2012-11-22 00:04:43 +0900

    Add server implementation

--- lib/gqtp/connection/thread.rb
+++ lib/gqtp/connection/thread.rb
@@ -21,36 +21,7 @@ require "thread"
 
 module GQTP
   module Connection
-    class Thread
-      attr_accessor :host, :port
-      def initialize(options={})
-        @options = options
-        @host = options[:host] || "127.0.0.1"
-        @port = options[:port] || 10041
-        @socket = TCPSocket.open(@host, @port)
-      end
-
-      def write(*chunks)
-        thread = ::Thread.new do
-          chunks.each do |chunk|
-            until chunk.empty?
-              written_bytes = @socket.write(chunk)
-              break if chunk.bytesize == written_bytes
-              chunk = chunk[written_bytes..-1]
-            end
-          end
-          yield if block_given?
-        end
-        Request.new(thread)
-      end
-
-      def read(size=nil)
-        thread = ::Thread.new do
-          yield(@socket.read(size)) if block_given?
-        end
-        Request.new(thread)
-      end
-
+    module Thread
       class Request
         def initialize(thread)
           @thread = thread
@@ -58,6 +29,88 @@ module GQTP
 
         def wait
           @thread.join
+        end
+      end
+
+      class IO
+        def initialize(real_io)
+          @real_io = real_io
+        end
+
+        def write(*chunks)
+          thread = ::Thread.new do
+            chunks.each do |chunk|
+              until chunk.empty?
+                written_bytes = @real_io.write(chunk)
+                break if chunk.bytesize == written_bytes
+                chunk = chunk[written_bytes..-1]
+              end
+            end
+            yield if block_given?
+          end
+          Request.new(thread)
+        end
+
+        def read(size=nil)
+          thread = ::Thread.new do
+            yield(@real_io.read(size)) if block_given?
+          end
+          Request.new(thread)
+        end
+
+        def close
+          @real_io.close
+        end
+      end
+
+      class Client
+        attr_accessor :host, :port
+        def initialize(options={})
+          @options = options
+          @host = options[:host] || "127.0.0.1"
+          @port = options[:port] || 10041
+          @socket = TCPSocket.open(@host, @port)
+          @io = IO.new(@socket)
+        end
+
+        def write(*chunks, &block)
+          @io.write(*chunks, &block)
+        end
+
+        def read(size=nil, &block)
+          @io.read(size, &block)
+        end
+
+        def close
+          @io.close
+        end
+      end
+
+      class Server
+        attr_accessor :host, :port
+        def initialize(options={})
+          @options = options
+          @host = options[:host] || "0.0.0.0"
+          @port = options[:port] || 10041
+          @backlog = options[:backlog] || 128
+        end
+
+        def run
+          @server = TCPServer.new(@host, @port)
+          @server.listen(@backlog)
+          thread = ::Thread.new do
+            loop do
+              client = @server.accept
+              ::Thread.new do
+                yield(IO.new(client))
+              end
+            end
+          end
+          Request.new(thread)
+        end
+
+        def shutdown
+          @server.shutdown
         end
       end
     end
---
+++ lib/gqtp/server.rb
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require "gqtp/header"
+
+module GQTP
+  class Server
+    attr_accessor :host, :port
+    def initialize(options={})
+      @options = options.dup
+      @options[:host] ||= "0.0.0.0"
+      @options[:port] ||= 10041
+      @on_request = nil
+    end
+
+    def run
+      @connection = create_connection
+      @connection.run do |client|
+        read_header_request = client.read(Header.size) do |header|
+          request_header = Header.parse(header)
+          read_body_request = client.read(request_header.size) do |body|
+            request = Request.new(request_header, body)
+            on_request(request, client)
+          end
+        end
+      end
+    end
+
+    def shutdown
+      @connection.shutdown
+    end
+
+    def on_request(*arguments, &block)
+      if block_given?
+        @on_request = block
+      else
+        request, client = arguments
+        @on_request.call(request, client)
+      end
+    end
+
+    private
+    def create_connection
+      connection = @options[:connection] || :thread
+      case connection
+      when :thread
+        require "gqtp/connection/#{connection}"
+        module_name = connection.to_s.capitalize
+        connection_module = GQTP::Connection::const_get(module_name)
+        connection_module::Server.new(@options)
+      else
+        raise "unknown connection: <#{connection.inspect}>"
+      end
+    end
+
+    class Request
+      attr_reader :header, :body
+      def initialize(header, body)
+        @header = header
+        @body = body
+      end
+    end
+  end
+end
