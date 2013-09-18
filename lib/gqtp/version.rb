ecef6eada47c9248181244e9665cf93a3b90a6ea
Kouhei Sutou <kou@clear-code.com>
2013-09-18 17:45:15 +0900

    Bump version
    1.0.5 ->
    1.0.6
        ^

--- lib/gqtp/version.rb
+++ lib/gqtp/version.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2012  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2012-2013  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,5 +17,5 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 module GQTP
-  VERSION = "1.0.5"
+  VERSION = "1.0.6"
 end
